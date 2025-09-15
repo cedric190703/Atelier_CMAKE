2c7da253a3f2c7e3733e53c6fa3dd67b78caa92d
Cedric_b <cedric.brzyski@epita.fr>
2025-09-15 15:08:30 +0200

    Update main script

--- src/main.c
+++ src/main.c
@@ -3,36 +3,43 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (argc == 3) {
-		char* op = argv[1];
-		char* a = argv[2];
-		double r = _square(atof(a));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 4) {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+        } else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+        } else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+        } else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+        } else {
+            printf("Erreur : opération inconnue\n");
+            return 1;
+        }
+        printf("%lf\n", r);
+
+    } else if (argc == 3) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "square") == 0) {
+            r = _square(atof(a));
+        } else {
+            printf("Erreur : opération inconnue\n");
+            return 1;
+        }
+        printf("%lf\n", r);
+    } else {
+        printf("Erreur de paramètres\n");
+        return 1;
+    }
+
+    return 0;
 }
